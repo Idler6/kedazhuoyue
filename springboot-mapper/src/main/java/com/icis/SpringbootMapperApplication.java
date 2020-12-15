8914470e3a76635f1f31b22e8e2a8cf2c1db3d22
Idler6 <1911039847@qq.com>
2020-12-15 16:00:02 +0800

    3.依赖、properties文件等配置

---
+++ springboot-mapper/src/main/java/com/icis/SpringbootMapperApplication.java
@@ -0,0 +1,16 @@
+package com.icis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+//开启包扫描
+@MapperScan(basePackages = "com.icis.mapper")
+public class SpringbootMapperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMapperApplication.class, args);
+    }
+
+}
